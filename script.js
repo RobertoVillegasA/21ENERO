15d592886bbc3bfd97a26fcc949ce9a593dcf0e5
RobertoVillegasA <villegas.roberto@gmail.com>
2024-12-07 04:37:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Crear el mapa
-const mapa = L.map('mapa').setView([40.416775, -3.703790], 13); // Coordenadas de Madrid, España
-
-// Añadir un mapa base (OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mapa);
-
-// Añadir un marcador
-const marcador = L.marker([40.416775, -3.703790]).addTo(mapa);
-marcador.bindPopup("<b>Madrid</b><br>Capital de España").openPopup();
+function loadHTML(filePath) {
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) throw new Error("HTML file not found.");
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => {
+            document.getElementById('content').innerHTML = `<p style="color: red;">Error: ${error.message}</p>`;
+        });
+}
